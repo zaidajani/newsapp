217650ed9a9fb88c1f75072790ee8d1c9b1cc4e6
zaidajani <zaidajani2007@gmail.com>
2021-11-05 23:47:53 +0530

    added many functionalities

--- src/components/News.js
+++ src/components/News.js
@@ -1,17 +1,96 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
 
 export class News extends Component {
+  articles = [
+    {
+      source: {
+        id: "bbc-sport",
+        name: "BBC Sport",
+      },
+      author: "BBC Sport",
+      title: "Yorkshire cricket racism scandal - how we got here",
+      description:
+        "BBC Sport timelines the stages of the Azeem Rafiq racism scandal which has engulfed Yorkshire County Cricket Club and English cricket.",
+      url: "http://www.bbc.co.uk/sport/cricket/59166142",
+      urlToImage:
+        "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/14229/production/_121437428_shutterstock_editorial_8827466h.jpg",
+      publishedAt: "2021-11-04T21:07:27.8995717Z",
+      content:
+        "Azeem Rafiq became the youngest man to captain Yorkshire in 2012\r\nIt is the scandal that has engulfed cricket.\r\nOne of England's most historic sporting clubs embroiled in a damning episode, accused o… [+6113 chars]",
+    },
+    {
+      source: {
+        id: "espn-cric-info",
+        name: "ESPN Cric Info",
+      },
+      author: null,
+      title:
+        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      description:
+        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      urlToImage:
+        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      publishedAt: "2020-04-27T11:41:47Z",
+      content:
+        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+    },
+    {
+      source: {
+        id: "espn-cric-info",
+        name: "ESPN Cric Info",
+      },
+      author: null,
+      title:
+        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      description:
+        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      urlToImage:
+        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      publishedAt: "2020-03-30T15:26:05Z",
+      content:
+        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+    },
+  ];
+  constructor() {
+    super();
+    console.log("hello bro, I am a constructor from news component");
+    this.state = {
+      articles: this.articles,
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=27112c5b82de4da2b0860df903f501d6";
+    let data = await fetch(url);
+    let parsedData = data.json();
+    console.log(parsedData);
+  }
+
   render() {
     return (
-      <div>
-        This is a news component.
-        <Newsitem />
+      <div className="container my-3">
+        <h2>NewsMonkey - top headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title.slice(0, 30) + " ..."}
+                  description={element.description.slice(0, 88) + "   ..."}
+                  imageurl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-
-
-export default News
+export default News;
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
+    let {title, description, imageurl, newsUrl} = this.props
     return (
-      <div>
-        This is the newsitem component
+      <div className="my-3">
+        <div className="card" style={{width: "100%"}}>
+          <img src={imageurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+              Read more
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
